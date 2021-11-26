86e7949e0db6f8d9bc7537f59022e7c9c9b0d58f
N4T4N99 <88665938+N4T4N99@users.noreply.github.com>
2021-11-26 10:04:52 -0300

    9commit

--- SITE/pagamento.php
+++ SITE/pagamento.php
@@ -1,37 +1,76 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-<meta charset="UTF-8">
-<title> TrueGaming</title>
-<link rel="stylesheet" href="stylegeral.css"> 
-<style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagamento</title>
+    <style> 
+    body{background:rgb(220, 220, 220);}
+    .lema{Position:absolute;top:1800px; left:45%;}
+    .divprincipal{background:white; width:1200px; height:1500px;position:absolute; top:150px; bottom:160px; left:center;}
+    .nomediv{background-color: rgb(220, 220, 220); width:310px; height:80px; border-radius: 10%;position:absolute; top:30px; left:150px;}
+    .nome{background-color: rgb(220, 220, 220); width:300px; height:30px;}
+    .sobrendiv{background-color: rgb(220, 220, 220); width:310px; height:80px; border-radius: 10%;position:absolute; top:30px; left:680px;}
+    .sobre{background-color: rgb(220, 220, 220); width:300px; height:30px;}
+    .enddiv{background-color: rgb(220, 220, 220); width:770px; height:80px; border-radius: 10%;position:absolute; top:160px; left:150px;}
+    .end{background-color: rgb(220, 220, 220); width:750px; height:30px;}
+    .teldiv{background-color: rgb(220, 220, 220); width:310px; height:80px; border-radius: 10%;position:absolute; top:320px; left:150px;}
+    .tel{background-color: rgb(220, 220, 220); width:300px; height:30px;}
+    .cartão{position:absolute; top:590px; left:45%;}
+    .ncarddiv{background-color: rgb(220, 220, 220); width:310px; height:80px; border-radius: 10%;position:absolute; top:720px; left:150px;}
+    .ncard{background-color: rgb(220, 220, 220); width:300px; height:30px;}
+    .nomecarddiv{background-color: rgb(220, 220, 220); width:310px; height:80px; border-radius: 10%;position:absolute; top:720px; right:150px;}
+    .nomecard{background-color: rgb(220, 220, 220); width:300px; height:30px;}
+    .codediv{background-color: rgb(220, 220, 220); width:310px; height:80px; border-radius: 10%;position:absolute; top:870px; left:150px;}
+    .code{background-color: rgb(220, 220, 220); width:300px; height:30px;}
+    .bandeiradiv{background-color: rgb(220, 220, 220); width:310px; height:40px; border-radius: 10%;position:absolute; top:890px; right:150px;}
+    .botão{font-size:20px;position:absolute; bottom:-400px;border-radius: 50%; background:red; width:150px; height:60px;right:340px;}
+   </style>
 
-
-    
-
-</style>
 </head>
+
+
 <body>
-<div id="superior"> 
-     <img id="logo"src="imgs/logomark.png" alt="logo"> <a href="paglogin.php"><input class="buttonlogin"type="button" value="Login"></a><a href="pagregistrar.php"><input class="buttonregister"type="button" value="Registrar"></a>
-     <div id="dentrodiv"> <img src="imgs/lupa.png" class="lupa"><input class="pesquisabar"><a href="index.php"> <span id="cabecalhoinicial"> Inicial </span> </a>
-     <a href="index.php"><span id="cabecalho"> Plataformas &#9660; <span> </a>
-     <a href="PLATAFORMAS/LANCAMENTOS/paglancamentos.php"> <span id="cabecalho2"> Lançamentos <span> </a><a href="PLATAFORMAS/PROMOCOES/pagpromocoes.php"> <span id="cabecalho3"> Promoçoes <span> </a>
-     <a href="pagsuporte.php"> <span id="cabecalho4"> Suporte <span> </a>
-    </div>  
-</div>  
-<H1> PAG TEST</h1>
 
+    <form action="pagamento2.php" method="post">
+<div class="divprincipal">
+<div class="nomediv">
+Nome:<input class="nome"type="text" required placeholder="Digite Seu Nome" name="firtsn">
+</div>
+<div class="sobrendiv">
+    Sobrenome:<input class="sobre"type="text" required required placeholder="Digite Seu Sobrenome" name="sn">
+    </div>   
+    <div class="enddiv">
+       Endereço:<input class="end"type="text" required required placeholder="Digite Seu Endereço" name="end">
+        </div> 
+        <div class="teldiv">
+            Telefone:<input class="tel"type="number" required required placeholder="Digite Telefone" name="tel">
+             </div> 
+             
+            <h1 class="cartão">-Cartão-</h1>
 
-<?php
-echo "aaa"
-?>
-<footer class="rodapepag"> <p> Loja Virtual TrueGaming<sup>&reg;</sup> </p>
-Todos Os Direitos Reservados. 
-</footer>
+            <div class="ncarddiv">
+                Numero Do Cartão:<input class="ncard"type="number" required required placeholder="Digite o numero do Cartão" name="numcd">
+                 </div> 
+                 <div class="nomecarddiv">
+                    Nome Titular:<input class="nomecard"type="text" required required placeholder="Nome do Titular do Cartão" name="ncd">
+                     </div> 
+                     <div class="codediv">
+                        Codigo de Segurança:<input class="code"type="number" required required placeholder="Codigo Segurança (atrás)" name="codback">
+                         </div> 
+                         <div class="bandeiradiv" name="flag">
+                            Selecione a Bandeira :<select>
+                                <option value="SP">  Visa </option>
+                  <option value="RJ">  MasterCard </option>
+                  <option value="MG">  Hipercard </option>
+                  <option value="RS">  Maestro </option>
+                            </select>
+
+                            <input type="submit" value="Confirmar" class="botão"> 
+ 
+</div>
+</form>
+<span class="lema">True Gaming Direitos Reservados<br> </span>
 </body>
-
 </html>
-
-
-
---
+++ SITE/pagamento2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagamento</title>
+    <style> 
+    body{background:rgb(220, 220, 220);}
+    .lema{Position:absolute;top:1800px; left:45%;}
+    .divprincipal{background:white; width:1200px; height:1500px;position:absolute; top:150px; bottom:160px; left:center;}
+    .msg{font-size:60px; color:green; position:absolute; top:700px; left:100px;}
+   </style>
+
+</head>
+
+
+<body>
+<div class="divprincipal">
+ <h1 class="msg"> Pagamento Aprovado! <br> Em Breve Enviaremos As Informações do Pedido!</h1>
+ 
+</div>
+ 
+<span class="lema">True Gaming Direitos Reservados<br> </span>
+</body>
+</html>
